240641b6f6bca81f60077aff42a8f014e9349c51
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-21 08:51:57 +0100

    nie zawsze działa

--- diff.js
+++ diff.js
@@ -1,4 +1,25 @@
 https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
 function arrayDiff(a, b) {
-  
+    if (b.length <= 0 || a.length <= 0) {
+        console.log(a);
+        console.log(b);
+        return 0;
+    }
+
+    else {
+        let ib = 0
+        while (ib <= b.length) {
+            let porownanie = a.map((el, i) => {
+                if (el == b[ib]) a.splice(i, 1)
+            });
+            ib++;
+        }
+        console.log(a);
+        console.log(b);
+    }
+
 }
+
+//arrayDiff([], [4, 5]); // [], "a was [], b was [4,5]");
+arrayDiff([3, 4], [3]) // [4], "a was [3,4], b was [3]");
+//arrayDiff([1, 8, 2], []) // [1,8,2], "a was [1,8,2], b was []");
