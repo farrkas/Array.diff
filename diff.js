e52b28f6e1b4fc52d92767364aa3733bcb6f908d
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-21 11:39:05 +0100

    problem gdy liczba powtarza sie w tablicy a

--- diff.js
+++ diff.js
@@ -5,21 +5,21 @@ function arrayDiff(a, b) {
         console.log(b);
         return 0;
     }
-
-    else {
-        let ib = 0
-        while (ib <= b.length) {
-            let porownanie = a.map((el, i) => {
-                if (el == b[ib]) a.splice(i, 1)
-            });
-            ib++;
+    else {        
+        for (var ib=0; ib <= b.length; ib++) {
+            var porownane = a;
+            a.forEach((el, i) => {
+                if (el === b[ib]) {
+                    porownane.splice(i, 1);
+                }                
+            });                      
         }
-        console.log(a);
-        console.log(b);
+        console.log(ib);
+        console.log(porownane);
     }
-
 }
 
 //arrayDiff([], [4, 5]); // [], "a was [], b was [4,5]");
-arrayDiff([3, 4], [3]) // [4], "a was [3,4], b was [3]");
+//arrayDiff([3, 4], [3]); // [4], "a was [3,4], b was [3]");
 //arrayDiff([1, 8, 2], []) // [1,8,2], "a was [1,8,2], b was []");
+arrayDiff([4, 17, -10, 10, -10, -15, -3, 13, 1, -2, -9], [17, 1, -15, -9, 13, -2, 10, -10, 4]);
